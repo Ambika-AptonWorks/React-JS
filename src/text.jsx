e106668a1e5b8bcbe582d70a97a6fce3e42e85f5
Ambika <amika@aptonworks.com>
2022-03-16 14:32:04 +0530

    components as functions

---
+++ src/text.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function Hello() {
+    return <h1>Hello, from Ambika
+        <p>This is my first react project</p>
+    </h1>;
+} 
+const Hello_comp = <Hello />;
+export default Hello_comp;
