ef5b4b09ca1c694b98b923615a0db4fa41b635a2
Ambika <amika@aptonworks.com>
2022-03-16 14:46:16 +0530

    class as component

--- src/text.jsx
+++ src/text.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-function Hello() {
-    return <h1>Hello, from Ambika
-        <p>This is my first react project</p>
-    </h1>;
-} 
-const Hello_comp = <Hello />;
-export default Hello_comp;
+
+class Hello extends React. Component {
+    render() {
+      return <h1>Hello, from Ambika!</h1>;
+    }
+  }
+  export default Hello;
