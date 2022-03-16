683c04efdf69741625c2406775f156d6503719c7
Ambika <amika@aptonworks.com>
2022-03-16 15:50:02 +0530

    props  to function component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './text';
+import Hello_comp from './text';
 
 ReactDOM.render(
-   <Hello msg="welcome,this is the page created by ambika" />,
-    document.getElementById('root')
+  Hello_comp,
+  document.getElementById('root')
 );
 
--- src/text.jsx
+++ src/text.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-class Hello extends React.Component{
-    render(){
-        return<h1>{this.props.msg}</h1>
-    }
-}
-export default Hello
+function Hello(props) {
+    return <h1>{props.msg}</h1>;
+}  
+const Hello_comp = <Hello msg="Hello, welcome to my page!" />;
+export default Hello_comp;
